72e79c124a5c68e976e46b112e47ae4155e731e0
ben18mk <benchik2004@gmail.com>
2024-01-18 21:13:58 +0200

    Started creating LowerCaseMorseEncoder implementing MorseEncoderBase

--- encoder/abstract/morse_encoder_base.py
+++ encoder/abstract/morse_encoder_base.py
@@ -2,5 +2,5 @@ from abc import ABC, abstractclassmethod
 
 class MorseEncoderBase(ABC):
     @abstractclassmethod
-    def Encode(self, text: str) -> bytes:
+    def encode(self, text: str) -> bytes:
         pass
---
+++ encoder/lowercase_morse_encoder.py
@@ -0,0 +1,5 @@
+from abstract.morse_encoder_base import MorseEncoderBase
+
+class LowerCaseMorseEncoder(MorseEncoderBase):
+    def encode(self, text: str) -> bytes:
+        pass # TODO: Implement
