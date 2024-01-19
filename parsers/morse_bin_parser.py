55a3cbeae8aa4437711d21e7f136e095d5c72479
ben18mk <benchik2004@gmail.com>
2024-01-19 15:35:19 +0200

    morse_bin_parser: Created function parse_bin_to_morse()

--- parsers/morse_bin_parser.py
+++ parsers/morse_bin_parser.py
@@ -14,3 +14,12 @@ def parse_morse_to_bin(morse: str) -> str:
     bin_list = list(map(lambda item: morse_to_bin[item], list(morse)))
     result = ''.join(bin_list)
     return result + (8 - len(result) % 8) * '0' # 0 Right Padding
+
+
+def parse_bin_to_morse(bin_string: str) -> str:
+    two_bits = [bin_string[i:i+2] for i in range(0, len(bin_string), 2)]
+    morse_items = list(map(
+        lambda two_bit: bin_to_morse[two_bit],
+        two_bits
+    ))
+    return ''.join(morse_items)
