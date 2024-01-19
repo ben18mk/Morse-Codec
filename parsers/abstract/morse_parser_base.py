8c064b8afe5190dd980ba0974fdb0b4208d528b3
ben18mk <benchik2004@gmail.com>
2024-01-19 16:47:07 +0200

    MorseParserBase: morese_item_handler is now passed in the constructor

--- parsers/abstract/morse_parser_base.py
+++ parsers/abstract/morse_parser_base.py
@@ -2,12 +2,16 @@ from abc import ABC
 from consts.parser_consts import MORSE_SEPARATOR, MORSE_SPACE
 
 class MorseParserBase(ABC):
-    def parse_morse_to_text(morse: str, morse_item_handler: function) -> str:
+    def __init__(self, morse_item_handler: function):
+        self._morse_item_handler = morse_item_handler
+
+
+    def parse_morse_to_text(self, morse: str) -> str:
         morse_words = morse.split(MORSE_SPACE)
         parsed_words = list(map(
             lambda word: ''.join(
                 list(map(
-                    morse_item_handler,
+                    self._morse_item_handler,
                     word.split(MORSE_SEPARATOR)
                 ))
             ),
