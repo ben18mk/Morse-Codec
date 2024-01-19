e387a92e1869f4628ff517e1ca37bf41b9c5314d
ben18mk <benchik2004@gmail.com>
2024-01-19 16:36:35 +0200

    MorseParserBase has the logic of the parsing while it's implementing classes pass their way of implementation

--- parsers/abstract/morse_parser_base.py
+++ parsers/abstract/morse_parser_base.py
@@ -1,6 +1,17 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC
+from consts.parser_consts import MORSE_SEPARATOR, MORSE_SPACE
 
 class MorseParserBase(ABC):
-    @abstractclassmethod
-    def parse_morse_to_text(morse: str) -> str:
-        pass
+    def parse_morse_to_text(morse: str, morse_item_handler: function) -> str:
+        morse_words = morse.split(MORSE_SPACE)
+        parsed_words = list(map(
+            lambda word: ''.join(
+                list(map(
+                    morse_item_handler,
+                    word.split(MORSE_SEPARATOR)
+                ))
+            ),
+            morse_words
+        ))
+        result = ' '.join(parsed_words)
+        return result
