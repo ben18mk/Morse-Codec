3d76f04f271c9aac7325b4edd964c4df7bb6daec
ben18mk <benchik2004@gmail.com>
2024-01-19 18:14:11 +0200

    collection_util: Refactored reversed_dict()

--- utils/collection_utils.py
+++ utils/collection_utils.py
@@ -1,2 +1,2 @@
-# CR: Clean-Code - instead of lambda define a function
-reversed_dict = lambda dictionary: { value: key for key, value in dictionary.items() }
+def reversed_dict(dictionary: dict) -> dict:
+    return { value: key for key, value in dictionary.items() }
