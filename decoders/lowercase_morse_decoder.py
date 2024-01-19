f01e2e1371a18e59f90a3af56155a36b0c0be264
ben18mk <benchik2004@gmail.com>
2024-01-19 15:30:19 +0200

    LowercaseMorseDecoder: converted the data bytes to a binary string

--- decoders/lowercase_morse_decoder.py
+++ decoders/lowercase_morse_decoder.py
@@ -2,4 +2,10 @@ from decoders.abstract.morse_decoder_base import MorseDecoderBase
 
 class LowercaseMorseDecoder(MorseDecoderBase):
     def decode(self, data: bytes) -> str:
+        data_hex = hex(int.from_bytes(data, 'big'))[2:]
+        bin_list = list(map(
+            lambda x: bin(int(x, 16))[2:].rjust(4, '0'),
+            list(data_hex)
+        ))
+        binary = ''.join(bin_list)
         pass # TODO: Implement
