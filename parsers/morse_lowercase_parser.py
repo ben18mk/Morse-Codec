68829af01d253cbf7c0bedfb7429c48986906eae
ben18mk <benchik2004@gmail.com>
2024-01-19 15:50:26 +0200

    Started implementing MorseParserBase with MorseLowercaseParser

---
+++ parsers/morse_lowercase_parser.py
@@ -0,0 +1,5 @@
+from parsers.abstract.morse_parser_base import MorseParserBase
+
+class LowercaseMorseParser(MorseParserBase):
+    def parse_morse_to_text(morse: str) -> str:
+        pass # TODO: Implement
