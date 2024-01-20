a337f62a2d91fe4a159e9528de153608a3b77218
ben18mk <benchik2004@gmail.com>
2024-01-20 15:33:21 +0200

    MorseEncoder: Replaced magic string with SYNTAX_ERROR consts dict

--- encoders/morse_encoder.py
+++ encoders/morse_encoder.py
@@ -1,3 +1,4 @@
+from consts.exceptions import SYNTAX_ERROR
 from consts.parser_consts import MORSE_SEPARATOR, MORSE_SPACE
 from encoders.abstract.morse_encoder_base import MorseEncoderBase
 from parsers.text_morse_parsers.conversion_dictionaries.text_morse_dictionaries import char_to_morse
@@ -7,15 +8,15 @@ from utils.string_mapping_utils import map_chars_through_words, map_whether_char
 class MorseEncoder(MorseEncoderBase):
     def encode(self, text: str) -> bytes:
         if not all(map_whether_chars_in_collection(text, char_to_morse.keys())):
-            raise SyntaxError('Text contains not supported characters')
+            raise SyntaxError(SYNTAX_ERROR['not_supported_chars'])
+        
         morse = map_chars_through_words(
             text,
             ' ',
             None,
             MORSE_SPACE,
             MORSE_SEPARATOR,
-            lambda char: char_to_morse[char.upper()]
-        )
+            lambda char: char_to_morse[char.upper()])
         morse_bin = '0b' + parse_morse_to_bin(morse)
         result = bytes.fromhex(hex(int(morse_bin, 2))[2:])
         return result
