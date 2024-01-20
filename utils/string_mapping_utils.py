dc7a161a3e3ad5a76671a7a85bbb05116f8b0e7e
ben18mk <benchik2004@gmail.com>
2024-01-20 15:26:13 +0200

    string_mapping_utils: Added function map_whether_chars_in_collection()

--- utils/string_mapping_utils.py
+++ utils/string_mapping_utils.py
@@ -19,3 +19,6 @@ def map_chars_through_words(
         words
     ))
     return new_words_delimiter.join(mapped_words)
+
+def map_whether_chars_in_collection(text: str, collection: list):
+    return list(map(lambda char: char in collection, collection))
